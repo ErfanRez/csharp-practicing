a0d5dd6ed1809418ba75473c08a97eb8fc3578f6
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-03-28 00:23:40 +0330

    Role aggregate added

---
+++ Projects/EShop/Shop/Shop.Domain/RoleAgg/Role.cs
@@ -0,0 +1,36 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace Shop.Domain.RoleAgg
+{
+    public class Role : AggregateRoot
+    {
+        public Role(string title, List<RolePermission> permissions)
+        {
+            NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+            Title = title;
+            Permissions = permissions;
+        }
+
+        public Role(string title)
+        {
+            NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+            Title = title;
+            Permissions = new List<RolePermission>();
+        }
+
+        public string Title { get; set; }
+        public List<RolePermission> Permissions { get; set; }
+
+        public void Edit(string title)
+        {
+            NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+            Title = title;
+        }
+        public void SetPermissions(List<RolePermission> permissions)
+        {
+            Permissions = permissions;
+        }
+    }
+
+}
