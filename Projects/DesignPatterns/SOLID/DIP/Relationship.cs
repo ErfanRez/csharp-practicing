31866f5486461a6efd95dd142e7afa2b76fe04cb
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-06-05 00:03:43 +0330

    Inversion of control principle

---
+++ Projects/DesignPatterns/SOLID/DIP/Relationship.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLID.DIP;
+
+// hl modules should not depend on low-level; both should depend on abstractions
+// abstractions should not depend on details; details should depend on abstractions
+
+public enum Relationship
+{
+    Parent,
+    Child,
+    Sibling
+}
