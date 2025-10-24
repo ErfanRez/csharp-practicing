be2b30ebd8a81baf7d9427a899f9d0812a633115
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-10-25 00:45:14 +0330

    project added to repo.

---
+++ Projects/etl-server/src/ETL.Application/Common/Constants/Policy.cs
@@ -0,0 +1,17 @@
+﻿namespace ETL.Application.Common.Constants;
+
+public static class Policy
+{
+    public const string CanCreateUser = "CanCreateUser";
+    public const string CanReadAllUsers = "CanReadAllUsers";
+    public const string CanChangeUserRole = "CanChangeUserRole";
+    public const string CanDeleteUser = "CanDeleteUser";
+    public const string CanReadUser = "CanReadUser";
+    public const string CanReadRoles = "CanReadRoles";
+    public const string CanUploadFile = "CanUploadFile";
+    public const string CanReadDataSets = "CanReadDataSets";
+    public const string CanManipulateDataSets = "CanManipulateDataSets";
+    public const string CanManageWorkflows = "CanManageWorkflows";
+    public const string CanViewWorkflows = "CanViewWorkflows"; 
+    
+}
---
+++ Projects/etl-server/src/ETL.Infrastructure/Security/AuthorizationExtension.cs
@@ -0,0 +1,50 @@
+﻿using ETL.Application.Common.Constants;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETL.Infrastructure.Security;
+
+public static class AuthorizationExtension
+{
+    public static IServiceCollection AddPolicyAuthorization(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(Policy.CanCreateUser, policy =>
+                policy.RequireRole(Role.SystemAdmin));
+
+            options.AddPolicy(Policy.CanReadAllUsers, policy =>
+                policy.RequireRole(Role.SystemAdmin));
+
+            options.AddPolicy(Policy.CanChangeUserRole, policy =>
+                policy.RequireRole(Role.SystemAdmin));
+
+            options.AddPolicy(Policy.CanDeleteUser, policy =>
+                policy.RequireRole(Role.SystemAdmin));
+
+            options.AddPolicy(Policy.CanReadUser, policy =>
+                policy.RequireRole(Role.SystemAdmin));
+
+            options.AddPolicy(Policy.CanReadRoles, policy =>
+                policy.RequireRole(Role.SystemAdmin));
+
+            options.AddPolicy(Policy.CanUploadFile, policy =>
+                policy.RequireRole(Role.SystemAdmin, Role.DataAdmin));
+
+            options.AddPolicy(Policy.CanReadDataSets, policy =>
+                policy.RequireRole(Role.SystemAdmin, Role.DataAdmin, Role.Analyst));
+
+            options.AddPolicy(Policy.CanManipulateDataSets, policy =>
+                policy.RequireRole(Role.SystemAdmin, Role.DataAdmin));
+
+            options.AddPolicy(Policy.CanViewWorkflows, policy =>
+                policy.RequireRole(Role.SystemAdmin, Role.DataAdmin, Role.Analyst));
+
+            options.AddPolicy(Policy.CanManageWorkflows, policy =>
+                policy.RequireRole(Role.SystemAdmin, Role.DataAdmin, Role.Analyst));
+
+        });
+
+        return services;
+    }
+}
