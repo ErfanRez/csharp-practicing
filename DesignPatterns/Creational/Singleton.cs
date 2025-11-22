1a24b2c1bfb73a9b91d13d9d05c0a75a82ef4c67
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-11-22 22:48:35 +0330

    Singleton pattern practiced.

---
+++ DesignPatterns/Creational/Singleton.cs
@@ -0,0 +1,44 @@
+﻿namespace Creational;
+public class Singleton
+{
+    public static void Main()
+    {
+        var obj = SingletonClass.Instance;
+
+        obj?.SomeBusinessLogic();
+    }
+}
+
+class SingletonClass
+{
+    private static SingletonClass? _instance;
+    private static object _lock = new object();
+
+    private SingletonClass()
+    {
+    }
+
+    public static SingletonClass? Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                // Thread safe
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonClass();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public void SomeBusinessLogic()
+    {
+        // Business logic here
+    }
+}
