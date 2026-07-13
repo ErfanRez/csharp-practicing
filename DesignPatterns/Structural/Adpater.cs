84eda8a9d1605afada3cc8f8944c783bf07bd2b4
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2026-07-13 21:43:18 +0330

    Adapter pattern practiced.

---
+++ DesignPatterns/Structural/Adpater.cs
@@ -0,0 +1,78 @@
+﻿namespace Structural;
+
+class AdapterProgram
+{
+    public static void Main()
+    {
+        ISmsProvider smsProvider = new SmsProvider();
+        IEnumerable<INotificationService> notificationServices = [new CustomNotificationService(), new SmsNotificationService(smsProvider)];
+        Dispatcher dispatcher = new(notificationServices);
+        dispatcher.SendNotification();
+    }
+
+}
+
+class Dispatcher
+{
+    private readonly IEnumerable<INotificationService> _notificationService;
+    public Dispatcher(IEnumerable<INotificationService> notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    public void SendNotification()
+    {
+        foreach (INotificationService notificationService in _notificationService)
+        {
+            notificationService.SendAsync("recipient", "subject", "message");
+        }
+    }
+}
+
+interface INotificationService
+{
+    void SendAsync(
+        string recipient,
+        string subject,
+        string message);
+}
+
+class CustomNotificationService : INotificationService
+{
+    public void SendAsync(string recipient, string subject, string message)
+    {
+        // notification processing logic
+    }
+}
+
+interface ISmsProvider
+{
+    void SendText(string phoneNumber, string text);
+}
+
+class SmsProvider : ISmsProvider
+{
+    public void SendText(
+        string phoneNumber,
+        string text)
+    {
+
+    }
+}
+
+// Adapter class
+class SmsNotificationService : INotificationService
+{
+    // Adaptee
+    private readonly ISmsProvider _smsProvider;
+
+    public SmsNotificationService(ISmsProvider smsProvider)
+    {
+        _smsProvider = smsProvider;
+    }
+
+    public void SendAsync(string recipient, string subject, string message)
+    {
+        _smsProvider.SendText(phoneNumber: recipient, text: message);
+    }
+}
